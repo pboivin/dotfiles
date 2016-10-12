bd20227933b7827daf199c79027775d26bed1d30
Patrick Boivin <pboivin@gmail.com>
2016-10-12 14:30:58 -0400

    Update install script

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+LINK="true"
 FILES=".bashrc .vimrc .vimrc-plugins .gitconfig .npmrc"
 
 for f in $FILES; do
@@ -7,8 +8,13 @@ for f in $FILES; do
     ORIG="$HOME/$f"
     BACKUP="$HOME/$f-bk-$(date +%s)"
 
-    if [ -e $ORIG ]; then
-        mv $ORIG $BACKUP 
+    if [ -e "$ORIG" ]; then
+        mv "$ORIG" "$BACKUP" 
     fi
-    ln -s $NEW $ORIG
+
+    if [ "$LINK" == "true" ]; then
+        ln -s "$NEW" "$ORIG"
+    else
+        cp -r "$NEW" "$ORIG"
+    fi
 done
