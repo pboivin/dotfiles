a848fef99be2efcf3acfd7ba7ffcd09915b5c6ed
Patrick Boivin <pboivin@gmail.com>
2019-03-21 11:41:11 -0400

    Add setup-tools.sh (wip)

---
+++ setup-tools.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+TOOLS_DIR="$HOME/Workspace/tools"
+
+if [ -e "$TOOLS_DIR" ]; then
+  echo "Error: $TOOLS_DIR already exists"
+  exit 1
+fi
+
+mkdir -p "$HOME/Workspace/tools"
+cd "$HOME/Workspace/tools"
+
+# Install z
+git clone https://github.com/rupa/z.git z
+
+echo
+echo
+echo "DONE!"
+echo
+echo "Please add the following to your bash configuration:"
+echo
+echo '. ~/Workspace/tools/z/z.sh'
+echo
