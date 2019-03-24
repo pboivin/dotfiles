c978a0ee67192e786e41d124e45e3ae20c162a06
Patrick Boivin <pboivin@gmail.com>
2019-03-24 17:23:27 -0400

    Update setup-tools.sh (wip)

--- setup-tools.sh
+++ setup-tools.sh
@@ -12,12 +12,24 @@ cd "$HOME/Workspace/tools"
 
 # Install z
 git clone https://github.com/rupa/z.git z
+git clone https://github.com/pboi20/fuzzycd.git
 
 echo
 echo
 echo "DONE!"
 echo
 echo "Please add the following to your bash configuration:"
-echo
-echo '. ~/Workspace/tools/z/z.sh'
+echo '
+# z
+
+. "$HOME/Workspace/tools/z/z.sh"
+
+# fuzzycd
+
+export FUZZYCD_DIR="$HOME/Workspace/tools/fuzzycd"
+. "$FUZZYCD_DIR/fuzzycd_helper.sh"
+alias f="fuzzycd"
+alias fa="fuzzycd -a"
+alias ff="fuzzycd -f"
+'
 echo
