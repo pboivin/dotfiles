fb75af5615f2408736f2b7422d67daa667dc4d6b
Patrick Boivin <pboivin@gmail.com>
2019-07-22 18:08:24 -0400

    Extract 'grepi' helper

---
+++ src/.bash-helpers/grepi.sh
@@ -0,0 +1,21 @@
+function grepi {
+    local cmd="grep -nri $1 ."
+    local i
+
+    if [ $# -lt 1 ]
+    then
+        echo "Error: need at least one argument"
+        return
+    fi
+
+    # build grep chain with function arguments
+    i=2
+    while [ $i -le $# ]
+    do
+        eval 'cmd="$cmd | grep -i $'$i'"'
+        let i+=1
+    done
+
+    # run grep chain
+    eval "$cmd"
+}
